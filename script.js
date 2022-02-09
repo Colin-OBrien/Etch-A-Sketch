3a618080f83b7275323c938727726ec9f64e39cf
Colin <colin.obrien590@gmail.com>
2022-02-08 20:00:22 -0500

    grid up

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
 function createGrid(size=16) {
-    const container = document.querySelector('#grid-container');
-    for (row=0; row < size; row++) {
-        let row = document.createElement('div');
-        row.className = 'row'
+    const container = document.querySelector('#grid-container'); //defining our container div and selecting it
+    for (y=0; y < size; y++) { //looping through the specified size and creating a div of class column
+        let column = document.createElement('div');
+        column.className = 'column'
 
-        for (x=1; x<=size; x++) {
+        for (x=1; x<=size; x++) { //within the column div we are looping through and adding a div of gridsquare
             let cell = document.createElement('div');
             cell.className = 'gridSquare';
-            cell.innerText = (row*size) + x;
-            row.appendChild(cell);
+            //cell.innerText = (y*size) + x; - removed the text and used mins on height and width in css
+            column.appendChild(cell); //adding our square to the column, this will iterate until size is met
         };
-        container.appendChild(row);
+        container.appendChild(column); //adding our column full of squares to the container
     };
 
 };
 
 
-createGrid();
+createGrid(16);
 
 
 
